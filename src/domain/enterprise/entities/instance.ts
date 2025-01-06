b29fe29974a7de866c51a532e909b7e78ee2f6dc
gustavodetoni <gustavodetoni2407@gmail.com>
2025-01-05 22:34:09 -0300

    refactor instance entity

--- src/domain/enterprise/entities/instance.ts
+++ src/domain/enterprise/entities/instance.ts
@@ -1,25 +1,55 @@
-import { Entity } from "src/core/entities/entity";
+import { Entity } from 'src/core/entities/entity'
+import { UniqueEntityID } from 'src/core/entities/unique-entity-id'
+import { Optional } from 'src/core/optinal'
 
 export type InstanceProps = {
-    name: string;
-    qrCode?: string;
-    connectionStatus?: string;
+  name: string
+  qrCode?: string
+  connectionStatus?: string
+  createAt: Date
+  upadateAt?: Date
 }
 
 export class Instance extends Entity<InstanceProps> {
-    get name() {
-        return this.props.name;
-    }
+  get name() {
+    return this.props.name
+  }
 
-    get qrCode() {
-        return this.props.qrCode;
-    }
+  get qrCode() {
+    return this.props.qrCode || undefined
+  }
 
-    get connectionStatus() {
-        return this.props.connectionStatus;
-    }
+  get connectionStatus() {
+    return this.props.connectionStatus || undefined
+  }
 
-    static create(props: InstanceProps) {
-        return new Instance(props);
-    }
+  get createAt() {
+    return this.props.createAt
+  }
+
+  get upadateAt() {
+    return this.props.upadateAt || undefined
+  }
+
+  updateQRCode(qrCode: string) {
+    this.props.qrCode = qrCode
+  }
+
+  updateConnectionStatus(status: string) {
+    this.props.connectionStatus = status
+  }
+
+  public static create(
+    props: Optional<InstanceProps, 'createAt'>,
+    id?: UniqueEntityID,
+  ) {
+    const instance = new Instance(
+      {
+        ...props,
+        createAt: props.createAt ?? new Date(),
+      },
+      id,
+    )
+    return instance
+  }
 }
